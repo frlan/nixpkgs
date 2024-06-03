918445146d50c73b774a23b80a4cf90cf1d141f8
Frank Lanitz <frank@frank.uvena.de>
2024-06-03 22:42:56 +0200

    Move homebank to by-name structure

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31505,10 +31505,6 @@ with pkgs;
 
   hmm = callPackage ../applications/misc/hmm { };
 
-  homebank = callPackage ../applications/office/homebank {
-    gtk = gtk3;
-  };
-
   hollywood = callPackage ../applications/misc/hollywood {
     inherit (python3Packages) pygments;
   };
